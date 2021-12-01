c59a443ac540d5f0aee3a0ccdf2d31532d7a8e38
kukulaj <jim.kukula@gmail.com>
2021-11-30 19:19:23 -0700

    53edo braid

--- meantone/Program.cs
+++ meantone/Program.cs
@@ -8,7 +8,7 @@ namespace meantone
     {
         static void Main(string[] args)
         {
-            Type_Map map = new Type_Map(new Random(5261));
+            Type_Map map = new Type_Map(new Random(5262));
 
             Work work;
 
