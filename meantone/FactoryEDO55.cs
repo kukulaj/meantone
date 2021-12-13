b03dd5d17704e155447905c542c96429c66bf237
kukulaj <jim.kukula@gmail.com>
2021-12-12 18:43:44 -0700

    55edo 24x24

---
+++ meantone/FactoryEDO55.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace meantone
+{
+    public class FactoryEDO55 : FactoryEDO5
+    {
+        private bool[] dichotomy;
+        public FactoryEDO55(Type_Map map, bool[] pinc) : base(55, map, pinc)
+        {
+            pumpStructure = new PumpStructureNull(this);
+
+            pumpStructure = new PumpStructureNull(this);
+            dichotomy = new bool[edo];
+            dichotomy[0] = true;
+            dichotomy[32] = true;
+            dichotomy[23] = true;
+            dichotomy[18] = true;
+            dichotomy[37] = true;
+            dichotomy[14] = true;
+            dichotomy[41] = true;
+            dichotomy[9] = true;
+            dichotomy[46] = true;
+        }
+
+        public override double vertical_interval_cost(int dp, int loc)
+        {
+            if (dp < 0)
+            {
+                dp = -dp;
+            }
+
+            if (dp % edo == 0)
+            {
+                return 100.0;
+            }
+            if (dichotomy[dp % edo])
+                return 0.0;
+            return 5000.0;
+        }
+    }
+
+}
--- meantone/Program.cs
+++ meantone/Program.cs
@@ -8,7 +8,7 @@ namespace meantone
     {
         static void Main(string[] args)
         {
-            Type_Map map = new Type_Map(new Random(5277));
+            Type_Map map = new Type_Map(new Random(5278));
 
             Work work;
 
@@ -29,7 +29,7 @@ namespace meantone
 
             Vertex.parallelism = 0.0;
             double temp = 10.0;
-            double target = 0.225;
+            double target = 0.095;
             for (int iter = 0; iter < 1; iter++)
             {
                 //work.voices[freeze].freeze = !fmode;
@@ -94,7 +94,7 @@ namespace meantone
                     }
                     else
                     {
-                        const double lower_lim = 600.0;
+                        const double lower_lim = 450.0;
                         while (temp > lower_lim && bfrac < target)
                         {
                             temp *= (1.0 - move);
