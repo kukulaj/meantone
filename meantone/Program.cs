af587eadc0c4c95a9a224045ff1d83caed6e9641
kukulaj <jim.kukula@gmail.com>
2021-12-31 21:44:47 -0700

    19edo 8x8

--- meantone/Program.cs
+++ meantone/Program.cs
@@ -8,7 +8,7 @@ namespace meantone
     {
         static void Main(string[] args)
         {
-            Type_Map map = new Type_Map(new Random(5281));
+            Type_Map map = new Type_Map(new Random(5282));
 
             Work work;
 
