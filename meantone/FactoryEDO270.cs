4884bad9fb06af70875c77612a33173e71c711a4
kukulaj <jim.kukula@gmail.com>
2022-01-06 22:01:29 -0700

    270edo 24x24

---
+++ meantone/FactoryEDO270.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace meantone
+{
+    public class FactoryEDO270 : FactoryEDO5
+    {
+
+        public FactoryEDO270(Type_Map map, bool[] pinc) : base(270, map, pinc)
+        {
+            build_dichotomy(11);
+            pumpStructure = new PumpStructureNull(this);
+        }
+    }
+}
