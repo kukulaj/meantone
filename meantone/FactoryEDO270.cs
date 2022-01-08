ab921abc19ce55bcfe233ee1368d67a3923d591c
kukulaj <jim.kukula@gmail.com>
2022-01-08 16:37:10 -0700

    270eo 3pump

--- meantone/FactoryEDO270.cs
+++ meantone/FactoryEDO270.cs
@@ -10,12 +10,21 @@ namespace meantone
         public FactoryEDO270(Type_Map map, bool[] pinc) : base(270, map, pinc)
         {
             build_dichotomy(13);
-            commas = new Comma[1];
-            pumps = new Pump[1];
+            commas = new Comma[4];
+            pumps = new Pump[4];
             commas[0] = new Comma(this, new int[] { 2, 2, 1, 3 });
             pumps[0] = new Pump(commas[0]);
+            commas[1] = new Comma(this, new int[] { 0, 1, 0, 3, -1 });
+            pumps[1] = new Pump(commas[0]);
+            commas[2] = new Comma(this, new int[] { 1, 2, -4});
+            pumps[2] = new Pump(commas[0]);
+            commas[3] = new Comma(this, new int[] { 2, 1, 1, 0, 1 });
+            pumps[3] = new Pump(commas[0]);
 
-            pumpStructure = new PumpStructureSimple(this, 0);
+            PumpStructure ps1 = new PumpStructureTwoAcross(this, 1, 3);
+            PumpStructure ps2 = new PumpStructureSimple(this, 2);
+
+            pumpStructure = new PumpStructureSum(this, ps1, ps2);
         }
     }
 }
--- meantone/Program.cs
+++ meantone/Program.cs
@@ -8,7 +8,7 @@ namespace meantone
     {
         static void Main(string[] args)
         {
-            Type_Map map = new Type_Map(new Random(5288));
+            Type_Map map = new Type_Map(new Random(5289));
 
             Work work;
 
@@ -30,7 +30,7 @@ namespace meantone
 
             Vertex.parallelism = 0.0;
             double temp = 10.0;
-            double target = 0.02;
+            double target = 0.021;
             for (int iter = 0; iter < 1; iter++)
             {
                 //work.voices[freeze].freeze = !fmode;
@@ -42,8 +42,8 @@ namespace meantone
                 }
 
                 //target = target * 0.97;
-                temp = 1000000.0;
-                work.jostle(temp, 2000);
+                //temp = 1000000.0;
+                //work.jostle(temp, 2000);
                 double bfrac = work.bfrac();
                 double afrac = work.align_count();
                 //temp = 140.0 - 5.0 * (double)iter;
@@ -73,7 +73,7 @@ namespace meantone
                 int effort = 1200;
                 //double target = 0.1;
 
-                bool up = false;
+                bool up = true;
                 int bounce = 0;
                 while (bounce < 1)
                 {
