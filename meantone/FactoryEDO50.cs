d9d6eee5bacbf5ab0a650da20216b158124f8d03
kukulaj <jim.kukula@gmail.com>
2022-02-17 16:28:07 -0700

    50edo adrift

--- meantone/FactoryEDO50.cs
+++ meantone/FactoryEDO50.cs
@@ -15,16 +15,19 @@ public class FactoryEDO50 : FactoryEDO5
         pumps[1] = new Pump(commas[1],
             new int[] {37, 16, 45, 24, 3 });
 
-        pumpStructure = new PumpStructureSimple(this, 1);
+        pumpStructure = new PumpStructureRandom(this);
 
+            scale = new bool[25];
+            /*
         for (int i = 0; i < edo; i++)
         {
             scale[i] = false;
         }
+            */
 
-        for (int i = 0; i < 12; i++)
+        for (int i = 0; i < 14; i++)
         {
-            scale[(29 * i) % edo] = true;
+            scale[(9 * i) % scale.Length] = true;
         }
 
         dichotomy = new bool[2, edo];
@@ -44,6 +47,7 @@ public class FactoryEDO50 : FactoryEDO5
 
     public override double vertical_interval_cost(int dp, int loc)
     {
+            return interval_cost(dp);
         if (dp < 0)
         {
             dp = -dp;
@@ -58,5 +62,26 @@ public class FactoryEDO50 : FactoryEDO5
             return 0.0;
         return 5000.0;
     }
-}
+        private bool inAScale(bool[] aScale, int pitch)
+        {
+            int period = aScale.Length;
+            int rem = pitch % period;
+            if (rem < 0)
+            {
+                rem = rem + period;
+            }
+            return aScale[rem];
+        }
+
+        public override bool inScale(int pitch, int loc)
+        {
+
+            bool result = true;
+
+
+            result = inAScale(scale, pitch - 29 * ((loc % 4)));
+
+            return result;
+        }
+    }
 }
--- meantone/Work.cs
+++ meantone/Work.cs
@@ -38,7 +38,7 @@ namespace meantone
             //primes[4] = true;
             //primes[5] = true;
 
-            vectorFactory = new FactoryEDO55( map, primes);
+            vectorFactory = new FactoryEDO50( map, primes);
             vectorFactory.show_pattern();
             vectorFactory.scaleSearch();
             rand = map.rand;
