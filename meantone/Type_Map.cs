dfeadb59d71a9827dda2d9262097980a22437de8
kukulaj <jim.kukula@gmail.com>
2022-11-04 20:00:22 -0700

    171edo 16x16

--- meantone/Type_Map.cs
+++ meantone/Type_Map.cs
@@ -23,15 +23,15 @@ namespace meantone
         public Type_Map(Random r)
         {
             file_prefix = @"C:\Users\James\Documents\tuning\meantone\";
-            row_size = 48;
-            dimension = 1;
+            row_size = 16;
+            dimension = 2;
             size = 1;
             for (int i = 0; i < dimension; i++)
             {
                 size = size * row_size;
             }
 
-            duration = 100.0;
+            duration = 20.0;
 
             drange = 4;
             rand = r;
